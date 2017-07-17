20473182fe43461944e5a7dc111ff972ee66e050
mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2017-07-17 02:06:44 +0200

    adding main_activity

---
+++ app/src/main/java/com/mahmoud_ashraf/y_t_s_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mahmoud_ashraf.y_t_s_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
